20f0d9981074c94840f55420ac9200edb9bf328c
yonin7 <yonin789@walla.com>
2021-12-06 13:55:28 +0200

    first uplaod

---
+++ 25.1/index.js
@@ -0,0 +1,16 @@
+const hero = {
+  health: 5,
+  power: 68,
+  getStrength: function () {
+    if (this.health <= 5) {
+      return this.power - 10;
+    } else return this.power;
+  },
+};
+function whoIsStronger(getStrength) {
+  const myStrength = 82;
+  if (getStrength() < myStrength) {
+    return 'I am stronger';
+  } else return 'You are stronger';
+}
+whoIsStronger(hero.getStrength.bind(hero));
